93e094a0398f2e00b2323c461976c58018581dd3
zamRL <zobayer.a.m@gmail.com>
2020-11-06 00:20:48 +0600

    test

--- src/My/Welcome.js
+++ src/My/Welcome.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 class Welcome extends Component{
     render () {
-        return <h1>This is from welcome class</h1>
+        return <h1>This is from a welcome class</h1>
     }
 }
 
